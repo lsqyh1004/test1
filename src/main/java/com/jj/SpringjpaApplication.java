1a06e0b8767bc3d73a225061d57390a402ebc3c3
zhanglirong123 <836659169@qq.com>
2019-11-30 14:24:59 +0800

    对启动类进行了修改

--- src/main/java/com/jj/SpringjpaApplication.java
+++ src/main/java/com/jj/SpringjpaApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringjpaApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringjpaApplication.class);
-        System.out.println("hahah");
+        System.out.println("aaaaaa");
     }
 }
